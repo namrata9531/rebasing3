30e235e6f29d1dea4bd93160f846091062744d10
namratakhobragade <namrata@theprocedure.in>
2022-10-17 00:32:24 +0530

    Added on pause method

--- app/src/main/java/com/example/rebasingdemo/MainActivity.kt
+++ app/src/main/java/com/example/rebasingdemo/MainActivity.kt
@@ -8,4 +8,8 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+    override fun onPause() {
+        super.onPause()
+    }
 }
